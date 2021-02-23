2b65034cdeebbe09f3020b6a2f979bbe4904b108
Jesus Acosta <jesusgabriel.91@gmail.com>
2021-02-22 23:44:09 -0400

    models: playlist: add comments

--- lib/models/playlist.dart
+++ lib/models/playlist.dart
@@ -3,16 +3,34 @@ import 'package:newpipeextractor_dart/models/infoItems/video.dart';
 
 class YoutubePlaylist {
 
+  /// Playlist ID
   String id;
+
+  /// Playlist name
   String name;
+
+  /// Playlist full URL
   String url;
+
+  /// Playlist authors name
   String uploaderName;
+
+  /// Playlist author avatar url
   String uploaderAvatarUrl;
+
+  /// Playlist channel url
   String uploaderUrl;
+
+  /// Playlist banner url
   String bannerUrl;
+
+  /// Playlist thumbnail url
   String thumbnailUrl;
+
+  /// Playlist videos ammount
   int streamCount;
 
+  /// Playlist streams (Videos)
   List<StreamInfoItem> streams;
 
   YoutubePlaylist(
@@ -27,10 +45,12 @@ class YoutubePlaylist {
     this.streamCount
   );
 
+  /// Retrieve this Playlist list of streams (List of StreamInfoItem)
   Future<void> getStreams() async {
     streams = await PlaylistExtractor.getPlaylistStreams(url);
   }
 
+  /// Retrieve more streams from the playlist
   Future<void> getMoreStreams() async {
     streams.addAll(await PlaylistExtractor.getNextPage());
   }
