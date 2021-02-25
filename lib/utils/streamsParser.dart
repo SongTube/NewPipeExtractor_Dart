40dba337f52c361d131c8fc83dac10aec4756b6b
Jesus Acosta <jesusgabriel.91@gmail.com>
2021-02-25 13:47:30 -0400

    utils: implement streamsParser

---
+++ lib/utils/streamsParser.dart
@@ -0,0 +1,23 @@
+import 'package:newpipeextractor_dart/models/infoItems/video.dart';
+
+class StreamsParser {
+
+  /// Retrieves a list of StreamInfoItem from the method channel response map
+  static List<StreamInfoItem> parseStreamListFromMap(info) {
+    List<StreamInfoItem> streams = [];
+    info.forEach((_, map) {
+      streams.add(StreamInfoItem(
+        map['url'],
+        map['name'],
+        map['uploaderName'],
+        map['uploaderUrl'],
+        map['uploadDate'],
+        map['thumbnailUrl'],
+        int.parse(map['duration']),
+        int.parse(map['viewCount'])
+      ));
+    });
+    return streams;
+  }
+
+}
