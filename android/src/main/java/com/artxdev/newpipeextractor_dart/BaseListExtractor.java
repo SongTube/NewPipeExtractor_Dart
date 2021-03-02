daf7a6303743393ac0c4cfb6be3e9ef8ee573add
Jesus Acosta <jesusgabriel.91@gmail.com>
2021-03-02 16:09:33 -0400

    remove unused classes

--- android/src/main/java/com/artxdev/newpipeextractor_dart/BaseListExtractor.java
+++
@@ -1,6 +0,0 @@
-package com.artxdev.newpipeextractor_dart;
-
-public interface BaseListExtractor extends BaseExtractor {
-    void relatedItems() throws Exception;
-    void moreRelatedItems() throws Exception;
-}
--- android/src/main/java/com/artxdev/newpipeextractor_dart/BaseSearchExtractor.java
+++
@@ -1,7 +0,0 @@
-package com.artxdev.newpipeextractor_dart;
-
-public interface BaseSearchExtractor extends BaseListExtractor {
-    void searchString() throws Exception;
-    void searchSuggestion() throws Exception;
-    void searchCorrected() throws Exception;
-}
