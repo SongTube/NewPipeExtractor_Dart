aff9e2576f4f81036d0cd3a6f07f08456e07644c
Jesus Acosta <jesusgabriel.91@gmail.com>
2021-03-02 16:05:10 -0400

    models: search: add dynamic list with all results on it

--- lib/models/search.dart
+++ lib/models/search.dart
@@ -22,7 +22,11 @@ class YoutubeSearch {
     this.searchVideos,
     this.searchChannels,
     this.searchPlaylists
-  });
+  }) {
+    dynamicSearchResultsList.addAll(searchChannels);
+    dynamicSearchResultsList.addAll(searchPlaylists);
+    dynamicSearchResultsList.addAll(searchVideos);
+  }
 
   /// Get next page content and include all results 
   /// in the current object of YoutubeSearch
@@ -31,6 +35,16 @@ class YoutubeSearch {
     searchVideos.addAll(newItems[0] as List<StreamInfoItem>);
     searchPlaylists.addAll(newItems[1] as List<PlaylistInfoItem>);
     searchChannels.addAll(newItems[2] as List<ChannelInfoItem>);
+    dynamicSearchResultsList.addAll(newItems[2]);
+    dynamicSearchResultsList.addAll(newItems[1]);
+    dynamicSearchResultsList.addAll(newItems[0]);
   }
 
+  /// Gives you a dynamic list with all results in it.
+  /// 
+  /// This means initial Channels will be at the top of the list, then, initial
+  /// Playlists will be added and then, anything else found (Channel, Playlist 
+  /// or Videos) will be placed in the order they were found
+  List<dynamic> dynamicSearchResultsList = [];
+  
 }
