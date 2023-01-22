75f92eb8be7ce75b2ece6358573e26dcad775058
joseph Omer <jodeveloper8@gmail.com>
2023-01-22 08:51:06 +0300

    update dependencies

--- lib/extractors/channels.dart
+++ lib/extractors/channels.dart
@@ -1,61 +1,56 @@
+import 'package:html/parser.dart' as parser;
+import 'package:http/http.dart' as http;
 import 'package:newpipeextractor_dart/exceptions/badUrlException.dart';
-import 'package:newpipeextractor_dart/models/channel.dart';
-import 'package:newpipeextractor_dart/models/infoItems/video.dart';
 import 'package:newpipeextractor_dart/newpipeextractor_dart.dart';
 import 'package:newpipeextractor_dart/utils/httpClient.dart';
 import 'package:newpipeextractor_dart/utils/reCaptcha.dart';
 import 'package:newpipeextractor_dart/utils/streamsParser.dart';
 import 'package:newpipeextractor_dart/utils/stringChecker.dart';
-import 'package:html/parser.dart' as parser;
-import 'package:http/http.dart' as http;
 
 class ChannelExtractor {
-
   /// Retrieve all ChannelInfo and
   /// build it into our own Model
   static Future<YoutubeChannel> channelInfo(String? url) async {
     if (url == null || StringChecker.hasWhiteSpace(url))
       throw BadUrlException("Url is null or contains white space");
-    Future<dynamic> task() => NewPipeExtractorDart.extractorChannel.invokeMethod('getChannel', {
-      "channelUrl": url
-    });
+    Future<dynamic> task() => NewPipeExtractorDart.extractorChannel
+        .invokeMethod('getChannel', {"channelUrl": url});
     var channel = await task();
     // Check if we got reCaptcha needed response
     channel = await ReCaptchaPage.checkInfo(channel, task);
     return YoutubeChannel(
-      id: channel['id'],
-      name: channel['name'],
-      url: channel['url'],
-      avatarUrl: channel['avatarUrl'],
-      bannerUrl: channel['bannerUrl'],
-      description: channel['description'],
-      feedUrl: channel['feedUrl'],
-      subscriberCount: int.parse(channel['subscriberCount'])
-    );
+        id: channel['id'],
+        name: channel['name'],
+        url: channel['url'],
+        avatarUrl: channel['avatarUrl'],
+        bannerUrl: channel['bannerUrl'],
+        description: channel['description'],
+        feedUrl: channel['feedUrl'],
+        subscriberCount: int.parse(channel['subscriberCount']));
   }
 
   /// Retrieve uploads from a Channel URL
   static Future<List<StreamInfoItem>> getChannelUploads(String url) async {
-    if (url == null || StringChecker.hasWhiteSpace(url))
+    if (StringChecker.hasWhiteSpace(url))
       throw BadUrlException("Url is null or contains white space");
-    Future<dynamic> task() => NewPipeExtractorDart.extractorChannel.invokeMethod(
-      'getChannelUploads', { "channelUrl": url }
-    );
+    Future<dynamic> task() => NewPipeExtractorDart.extractorChannel
+        .invokeMethod('getChannelUploads', {"channelUrl": url});
     var info = await task();
     // Check if we got reCaptcha needed response
     info = await ReCaptchaPage.checkInfo(info, task);
     return StreamsParser.parseStreamListFromMap(info);
   }
- 
+
   /// Retrieve high quality Channel Avatar URL
   static Future<String?> getAvatarUrl(String channelId) async {
     var url = 'https://www.youtube.com/channel/$channelId?hl=en';
     var client = http.Client();
-    var response = await client.get(Uri.parse(url), headers: ExtractorHttpClient.defaultHeaders);
+    var response = await client.get(Uri.parse(url),
+        headers: ExtractorHttpClient.defaultHeaders);
     var raw = response.body;
-    return parser.parse(raw)
-      .querySelector('meta[property="og:image"]')
-      ?.attributes['content'];
+    return parser
+        .parse(raw)
+        .querySelector('meta[property="og:image"]')
+        ?.attributes['content'];
   }
-
 }
--- lib/extractors/comments.dart
+++ lib/extractors/comments.dart
@@ -1,33 +1,29 @@
 import 'package:newpipeextractor_dart/exceptions/badUrlException.dart';
-import 'package:newpipeextractor_dart/models/comment.dart';
 import 'package:newpipeextractor_dart/newpipeextractor_dart.dart';
 import 'package:newpipeextractor_dart/utils/reCaptcha.dart';
 import 'package:newpipeextractor_dart/utils/stringChecker.dart';
 
 class CommentsExtractor {
-
   static Future<List<YoutubeComment>> getComments(String videoUrl) async {
-    if (videoUrl == null || StringChecker.hasWhiteSpace(videoUrl))
+    if (StringChecker.hasWhiteSpace(videoUrl))
       throw BadUrlException("Url is null or contains white space");
     List<YoutubeComment> comments = [];
-    Future<dynamic> task() => NewPipeExtractorDart.extractorChannel.invokeMethod('getComments', {
-      "videoUrl": videoUrl
-    });
+    Future<dynamic> task() => NewPipeExtractorDart.extractorChannel
+        .invokeMethod('getComments', {"videoUrl": videoUrl});
     var info = await task();
     // Check if we got reCaptcha needed response
     info = await ReCaptchaPage.checkInfo(info, task);
     info.forEach((key, map) {
       comments.add(YoutubeComment(
-        author: map['author'],
-        commentText: map['commentText'],
-        uploadDate: map['uploadDate'],
-        uploaderAvatarUrl: map['uploaderAvatarUrl'],
-        uploaderUrl: map['uploaderUrl'],
-        commentId: map['commentId'],
-        likeCount: int.parse(map['likeCount']),
-        hearted: map['hearted'].toLowerCase() == 'true',
-        pinned: map['pinned'].toLowerCase() == 'true'
-      ));
+          author: map['author'],
+          commentText: map['commentText'],
+          uploadDate: map['uploadDate'],
+          uploaderAvatarUrl: map['uploaderAvatarUrl'],
+          uploaderUrl: map['uploaderUrl'],
+          commentId: map['commentId'],
+          likeCount: int.parse(map['likeCount']),
+          hearted: map['hearted'].toLowerCase() == 'true',
+          pinned: map['pinned'].toLowerCase() == 'true'));
     });
     return comments;
   }
